9c7f438a56a348139120acb8c60afc129ec67db2
Roy Rivera <roy.fita23@gmail.com>
2023-02-14 17:02:45 -0600

    Adding storybook and more setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
+import theme from '@/styles/theme'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from '../styles/global'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Aragon POAP Claim</title>
+        <meta name="description" content="Aragon POAP Claim." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
---
+++ styles/global.ts
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  html,
+  body {
+    font-family: 'Inter', sans-serif;
+    font-weight: 400;
+    font-size: 62.5%; /* base 10px */
+    overscroll-behavior: none;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body {
+    position: relative;
+    min-height: 100vh;
+
+    @supports (height: 100dvh) {
+      min-height: 100dvh;
+    }
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`
