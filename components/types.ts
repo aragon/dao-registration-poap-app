32efe0d4aca7faf4a5a1ca56d47b75b49d7fe5ae
Roy Rivera <roy.fita23@gmail.com>
2023-03-01 20:44:15 -0600

    fix build (#5)

--- components/types.ts
+++ components/types.ts
@@ -1,207 +1,207 @@
-import { gql } from '@apollo/client'
-import * as Apollo from '@apollo/client'
-export type Maybe<T> = T | null
-export type InputMaybe<T> = Maybe<T>
-export type Exact<T extends { [key: string]: unknown }> = {
-  [K in keyof T]: T[K]
-}
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]?: Maybe<T[SubKey]>
-}
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]: Maybe<T[SubKey]>
-}
-const defaultOptions = {} as const
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+const defaultOptions = {} as const;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string
-  String: string
-  Boolean: boolean
-  Int: number
-  Float: number
-  DateTime: any
-}
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  DateTime: any;
+};
 
 export type ExpirationWarning = {
-  __typename?: 'ExpirationWarning'
-  claimCodesCount: Scalars['Int']
-  expiringInLessThanDays: Scalars['Int']
-}
+  __typename?: 'ExpirationWarning';
+  claimCodesCount: Scalars['Int'];
+  expiringInLessThanDays: Scalars['Int'];
+};
 
 /** POAP Event to create */
 export type ImportPoapEventInput = {
   /** External identifier */
-  externalId: Scalars['Int']
+  externalId: Scalars['Int'];
   /** Secret Code */
-  secretCode: Scalars['String']
-}
+  secretCode: Scalars['String'];
+};
 
 export type Mutation = {
-  __typename?: 'Mutation'
-  findOrCreateUser: User
-  importPoapEvent: PoapEvent
-  login: Scalars['Boolean']
-  mintPoap: PoapClaimCode
-  reassignPendingClaimCodes: ReassignPendingSyncResult
-}
+  __typename?: 'Mutation';
+  findOrCreateUser: User;
+  importPoapEvent: PoapEvent;
+  login: Scalars['Boolean'];
+  mintPoap: PoapClaimCode;
+  reassignPendingClaimCodes: ReassignPendingSyncResult;
+};
+
 
 export type MutationImportPoapEventArgs = {
-  data: ImportPoapEventInput
-}
+  data: ImportPoapEventInput;
+};
+
 
 export type MutationLoginArgs = {
-  data: SiweSessionInput
-}
+  data: SiweSessionInput;
+};
+
 
 export type MutationReassignPendingClaimCodesArgs = {
-  count: Scalars['Int']
-  creatorAddress?: InputMaybe<Scalars['String']>
-}
+  count: Scalars['Int'];
+  creatorAddress: InputMaybe<Scalars['String']>;
+};
 
 export type PendingDaoRegistrySync = {
-  __typename?: 'PendingDAORegistrySync'
-  creatorAddress: Scalars['String']
-  daoAddress: Scalars['String']
-  errorLocation: Scalars['String']
-}
+  __typename?: 'PendingDAORegistrySync';
+  creatorAddress: Scalars['String'];
+  daoAddress: Scalars['String'];
+  errorLocation: Scalars['String'];
+};
 
 /** PoapClaimCode */
 export type PoapClaimCode = {
-  __typename?: 'PoapClaimCode'
+  __typename?: 'PoapClaimCode';
   /** DAO Address */
-  daoAddress: Scalars['String']
-  event: PoapEvent
+  daoAddress: Scalars['String'];
+  event: PoapEvent;
   /** Unique identifier */
-  id: Scalars['Int']
+  id: Scalars['Int'];
   /** POAP QR Hash */
-  qrHash: Scalars['String']
+  qrHash: Scalars['String'];
   /** POAP Claim Code */
-  status: Scalars['String']
-}
+  status: Scalars['String'];
+};
 
 /** A POAP Event */
 export type PoapEvent = {
-  __typename?: 'PoapEvent'
+  __typename?: 'PoapEvent';
   /** Creation date */
-  createdAt: Scalars['DateTime']
+  createdAt: Scalars['DateTime'];
   /** Expiration date */
-  expiresAt: Scalars['DateTime']
+  expiresAt: Scalars['DateTime'];
   /** External identifier */
-  externalId: Scalars['Int']
+  externalId: Scalars['Int'];
   /** Unique identifier */
-  id: Scalars['Int']
+  id: Scalars['Int'];
   /** Image */
-  image: Scalars['String']
-}
+  image: Scalars['String'];
+};
 
 export type Query = {
-  __typename?: 'Query'
-  activePoapEvents: Array<PoapEvent>
-  allPoapEvents: Array<PoapEvent>
-  canClaimPoap: Scalars['Boolean']
-  isMinted: Scalars['Boolean']
-  mintedClaimCode?: Maybe<PoapClaimCode>
-  nonce: Scalars['String']
-  poapEvent: PoapEvent
-  statistics: Statistics
-}
+  __typename?: 'Query';
+  activePoapEvents: Array<PoapEvent>;
+  allPoapEvents: Array<PoapEvent>;
+  canClaimPoap: Scalars['Boolean'];
+  isMinted: Scalars['Boolean'];
+  mintedClaimCode: Maybe<PoapClaimCode>;
+  nonce: Scalars['String'];
+  poapEvent: PoapEvent;
+  statistics: Statistics;
+};
+
 
 export type QueryIsMintedArgs = {
-  qrHash: Scalars['String']
-}
+  qrHash: Scalars['String'];
+};
+
 
 export type QueryPoapEventArgs = {
-  externalId: Scalars['Int']
-}
+  externalId: Scalars['Int'];
+};
 
 export type ReassignPendingSyncResult = {
-  __typename?: 'ReassignPendingSyncResult'
-  resolved: Scalars['Int']
-  unresolved: Scalars['Int']
-}
+  __typename?: 'ReassignPendingSyncResult';
+  resolved: Scalars['Int'];
+  unresolved: Scalars['Int'];
+};
 
 export type SiweMessage = {
   /** Address */
-  address: Scalars['String']
+  address: Scalars['String'];
   /** Chain ID */
-  chainId: Scalars['Int']
+  chainId: Scalars['Int'];
   /** Domain */
-  domain: Scalars['String']
+  domain: Scalars['String'];
   /** Issued At */
-  issuedAt: Scalars['String']
+  issuedAt: Scalars['String'];
   /** Nonce */
-  nonce: Scalars['String']
+  nonce: Scalars['String'];
   /** Statement */
-  statement: Scalars['String']
+  statement: Scalars['String'];
   /** URI */
-  uri: Scalars['String']
+  uri: Scalars['String'];
   /** Version */
-  version: Scalars['String']
-}
+  version: Scalars['String'];
+};
 
 export type SiweSessionInput = {
   /** Message to sign */
-  message: SiweMessage
+  message: SiweMessage;
   /** SIWE Signature */
-  signature: Scalars['String']
-}
+  signature: Scalars['String'];
+};
 
 export type Statistics = {
-  __typename?: 'Statistics'
+  __typename?: 'Statistics';
   /** Number of poap codes assigned */
-  assignedClaimCodesCount: Scalars['Float']
+  assignedClaimCodesCount: Scalars['Float'];
   /** Number of poap codes available */
-  availableClaimCodesCount: Scalars['Float']
+  availableClaimCodesCount: Scalars['Float'];
   /** Expiration Warning Details */
-  expirationWarning: ExpirationWarning
+  expirationWarning: ExpirationWarning;
   /** Number of poap codes minted */
-  mintedClaimCodesCount: Scalars['Float']
+  mintedClaimCodesCount: Scalars['Float'];
   /** Pending DAO Registry Syncs */
-  pendingSyncs: Array<PendingDaoRegistrySync>
-}
+  pendingSyncs: Array<PendingDaoRegistrySync>;
+};
 
 /** User */
 export type User = {
-  __typename?: 'User'
+  __typename?: 'User';
   /** Wallet Address */
-  address: Scalars['String']
+  address: Scalars['String'];
   /** Unique identifier */
-  id: Scalars['Int']
-}
+  id: Scalars['Int'];
+};
+
+export type LoginMutationVariables = Exact<{
+  domain: Scalars['String'];
+  address: Scalars['String'];
+  nonce: Scalars['String'];
+  statement: Scalars['String'];
+  uri: Scalars['String'];
+  version: Scalars['String'];
+  chainId: Scalars['Int'];
+  issuedAt: Scalars['String'];
+  signature: Scalars['String'];
+}>;
+
+
+export type LoginMutation = { __typename?: 'Mutation', login: boolean };
+
+export type NonceQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type NonceQuery = { __typename?: 'Query', nonce: string };
+
+export type MintPoapMutationVariables = Exact<{ [key: string]: never; }>;
+
+
+export type MintPoapMutation = { __typename?: 'Mutation', mintPoap: { __typename?: 'PoapClaimCode', id: number, qrHash: string, daoAddress: string } };
+
 
 export const LoginDocument = gql`
-  mutation Login(
-    $domain: String!
-    $address: String!
-    $nonce: String!
-    $statement: String!
-    $uri: String!
-    $version: String!
-    $chainId: Int!
-    $issuedAt: String!
-    $signature: String!
-  ) {
-    login(
-      data: {
-        message: {
-          domain: $domain
-          address: $address
-          nonce: $nonce
-          statement: $statement
-          uri: $uri
-          version: $version
-          chainId: $chainId
-          issuedAt: $issuedAt
-        }
-        signature: $signature
-      }
-    )
-  }
-`
-export type LoginMutationFn = Apollo.MutationFunction<
-  LoginMutation,
-  LoginMutationVariables
->
+    mutation Login($domain: String!, $address: String!, $nonce: String!, $statement: String!, $uri: String!, $version: String!, $chainId: Int!, $issuedAt: String!, $signature: String!) {
+  login(
+    data: {message: {domain: $domain, address: $address, nonce: $nonce, statement: $statement, uri: $uri, version: $version, chainId: $chainId, issuedAt: $issuedAt}, signature: $signature}
+  )
+}
+    `;
+export type LoginMutationFn = Apollo.MutationFunction<LoginMutation, LoginMutationVariables>;
 
 /**
  * __useLoginMutation__
@@ -228,29 +228,18 @@ export type LoginMutationFn = Apollo.MutationFunction<
  *   },
  * });
  */
-export function useLoginMutation(
-  baseOptions?: Apollo.MutationHookOptions<
-    LoginMutation,
-    LoginMutationVariables
-  >
-) {
-  const options = { ...defaultOptions, ...baseOptions }
-  return Apollo.useMutation<LoginMutation, LoginMutationVariables>(
-    LoginDocument,
-    options
-  )
-}
-export type LoginMutationHookResult = ReturnType<typeof useLoginMutation>
-export type LoginMutationResult = Apollo.MutationResult<LoginMutation>
-export type LoginMutationOptions = Apollo.BaseMutationOptions<
-  LoginMutation,
-  LoginMutationVariables
->
+export function useLoginMutation(baseOptions?: Apollo.MutationHookOptions<LoginMutation, LoginMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<LoginMutation, LoginMutationVariables>(LoginDocument, options);
+      }
+export type LoginMutationHookResult = ReturnType<typeof useLoginMutation>;
+export type LoginMutationResult = Apollo.MutationResult<LoginMutation>;
+export type LoginMutationOptions = Apollo.BaseMutationOptions<LoginMutation, LoginMutationVariables>;
 export const NonceDocument = gql`
-  query Nonce {
-    nonce
-  }
-`
+    query Nonce {
+  nonce
+}
+    `;
 
 /**
  * __useNonceQuery__
@@ -267,43 +256,27 @@ export const NonceDocument = gql`
  *   },
  * });
  */
-export function useNonceQuery(
-  baseOptions?: Apollo.QueryHookOptions<NonceQuery, NonceQueryVariables>
-) {
-  const options = { ...defaultOptions, ...baseOptions }
-  return Apollo.useQuery<NonceQuery, NonceQueryVariables>(
-    NonceDocument,
-    options
-  )
-}
-export function useNonceLazyQuery(
-  baseOptions?: Apollo.LazyQueryHookOptions<NonceQuery, NonceQueryVariables>
-) {
-  const options = { ...defaultOptions, ...baseOptions }
-  return Apollo.useLazyQuery<NonceQuery, NonceQueryVariables>(
-    NonceDocument,
-    options
-  )
-}
-export type NonceQueryHookResult = ReturnType<typeof useNonceQuery>
-export type NonceLazyQueryHookResult = ReturnType<typeof useNonceLazyQuery>
-export type NonceQueryResult = Apollo.QueryResult<
-  NonceQuery,
-  NonceQueryVariables
->
+export function useNonceQuery(baseOptions?: Apollo.QueryHookOptions<NonceQuery, NonceQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<NonceQuery, NonceQueryVariables>(NonceDocument, options);
+      }
+export function useNonceLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<NonceQuery, NonceQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<NonceQuery, NonceQueryVariables>(NonceDocument, options);
+        }
+export type NonceQueryHookResult = ReturnType<typeof useNonceQuery>;
+export type NonceLazyQueryHookResult = ReturnType<typeof useNonceLazyQuery>;
+export type NonceQueryResult = Apollo.QueryResult<NonceQuery, NonceQueryVariables>;
 export const MintPoapDocument = gql`
-  mutation MintPoap {
-    mintPoap {
-      id
-      qrHash
-      daoAddress
-    }
+    mutation MintPoap {
+  mintPoap {
+    id
+    qrHash
+    daoAddress
   }
-`
-export type MintPoapMutationFn = Apollo.MutationFunction<
-  MintPoapMutation,
-  MintPoapMutationVariables
->
+}
+    `;
+export type MintPoapMutationFn = Apollo.MutationFunction<MintPoapMutation, MintPoapMutationVariables>;
 
 /**
  * __useMintPoapMutation__
@@ -321,21 +294,10 @@ export type MintPoapMutationFn = Apollo.MutationFunction<
  *   },
  * });
  */
-export function useMintPoapMutation(
-  baseOptions?: Apollo.MutationHookOptions<
-    MintPoapMutation,
-    MintPoapMutationVariables
-  >
-) {
-  const options = { ...defaultOptions, ...baseOptions }
-  return Apollo.useMutation<MintPoapMutation, MintPoapMutationVariables>(
-    MintPoapDocument,
-    options
-  )
-}
-export type MintPoapMutationHookResult = ReturnType<typeof useMintPoapMutation>
-export type MintPoapMutationResult = Apollo.MutationResult<MintPoapMutation>
-export type MintPoapMutationOptions = Apollo.BaseMutationOptions<
-  MintPoapMutation,
-  MintPoapMutationVariables
->
+export function useMintPoapMutation(baseOptions?: Apollo.MutationHookOptions<MintPoapMutation, MintPoapMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<MintPoapMutation, MintPoapMutationVariables>(MintPoapDocument, options);
+      }
+export type MintPoapMutationHookResult = ReturnType<typeof useMintPoapMutation>;
+export type MintPoapMutationResult = Apollo.MutationResult<MintPoapMutation>;
+export type MintPoapMutationOptions = Apollo.BaseMutationOptions<MintPoapMutation, MintPoapMutationVariables>;
